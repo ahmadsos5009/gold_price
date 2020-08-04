9b8211330dbf09e7b13c1e2134ad0fb333795137
asouqi <asouqi@atypon.com>
2020-08-04 11:48:47 +0300

    config

--- src/price/index.tsx
+++ src/price/index.tsx
@@ -5,29 +5,18 @@ import CountryData from "country-data";
 import { Container, Row, Col } from 'react-bootstrap';
 import PriceTable from './PriceTable';
 import PriceWeight from './PriceWeight';
-import MyChart from './graph';
 import Welcome from './Welcome';
-
-type History = (number | Date)[][]
 
 interface Main extends RouteComponentProps {
   price: number,
   date: Date,
-  historyData: History
 }
 
 const StyledContiner = styled(Container)`
    background-color: #f5f5fa;
 `;
 
-const getGraphData = (historyData: History) => {
-  if (historyData[historyData.length - 1][1] > historyData[historyData.length - 2][1])
-    return [{ data: [] }, { label: 'Price During Month', data: historyData }];
-  else
-    return [{ data: [] }, { data: [] }, { data: [] }, { label: 'Price During Month', data: historyData }];
-}
-
-const Main: React.FC<Main> = ({ price, date, historyData, location }) => {
+const Main: React.FC<Main> = ({ price, date, location }) => {
   const currency = location.pathname === "/" ? "USD" : location.pathname.replace("/", "");
   const countryCurrency = CountryData.currencies[currency].name;
 
@@ -42,11 +31,6 @@ const Main: React.FC<Main> = ({ price, date, historyData, location }) => {
             </Col>
             <Col>
               <PriceWeight price={price} />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <MyChart pricesHistory={getGraphData(historyData)} />
             </Col>
           </Row>
           <Row>
