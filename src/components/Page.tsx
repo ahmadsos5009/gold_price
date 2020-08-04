442d645d8909bd43e1ea03479599c1a4fd45b4b0
asouqi <asouqi@atypon.com>
2020-08-04 21:02:49 +0300

    config

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -3,14 +3,6 @@ import { Container } from "react-bootstrap";
 import Header from "./Header";
 import Main from "../price";
 
-const data = [
-  [new Date("2020,07,06"), 45.02],
-  [new Date("2020,07,05"), 43.61],
-  [new Date("2020,07,04"), 42.9],
-  [new Date("2020,07,03"), 41.65],
-  [new Date("2020,07,02"), 41.18],
-];
-
 export const Page: React.FC = () => (
   <Container fluid={"md"}>
     <Header />
