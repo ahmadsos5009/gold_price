2d1ebed0fb3efc3fdb4d7dc8feae1fe97c1242a2
ahmadSouqi <ahmadsos5009@hotmail.com>
2020-08-07 05:16:07 +0300

    google boot

---
+++ src/components/GooglePage.tsx
@@ -0,0 +1,5 @@
+import React, { Fragment } from 'react';
+
+export default () => (
+  <Fragment>google-site-verification: google86229b14d00bfabe.html</Fragment>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,15 @@ import Page from "./components/Page";
 import { CurrencyCode } from "./constants";
 
 ReactDOM.render(
-<Router>
-      <Switch>
-        <Route path="/" exact component={Page} />
-        {CurrencyCode.map((currency: string) => (
-          <Route
-            key={currency}
-            path={`/${currency}`}
-            exact
-            component={Page}
-          />
-        ))}
-        <Route component={() => <h1>Not Fond Page 404</h1>} />
-      </Switch>
-  </Router>
-  ,
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Page} />
+      <Route path="/google86229b14d00bfabe.html" exact component={Page} />
+      {CurrencyCode.map((currency: string) => (
+        <Route key={currency} path={`/${currency}`} exact component={Page} />
+      ))}
+      <Route component={() => <h1>Not Fond Page 404</h1>} />
+    </Switch>
+  </Router>,
   document.getElementById("root")
 );
